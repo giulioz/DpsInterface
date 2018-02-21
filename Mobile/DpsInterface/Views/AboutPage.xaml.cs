79e2ba9d1531275face64d19187488cd07c2fb59
giulioz <mail.zausa.giulio@gmail.com>
2018-02-21 12:44:16 +0100

    auto copy

---
+++ Mobile/DpsInterface/Views/AboutPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace DpsInterface
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
